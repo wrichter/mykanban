e0876fe0dcca27b50146a5a990565556a30514cc
Wolfram Richter <wrichter@redhat.com>
2015-01-25 00:42:12 +0100

    Commit from JBoss Tools

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
 #!/bin/env node
 
-var express = require('express');
-//var fs      = require('fs');
-var mongodb = require('mongodb');
-var path	= require('path');
-//var bodyParser 		= require ('body-parser');
-var route_index 	= require('./routes/index');
-//var route_users		= require('./routes/users');
-//var route_mkDropLink = require('./routes/mk-droplink');
+var express 		= require('express');
+//var fs      		= require('fs');
+var mongodb 		= require('mongodb');
+var path			= require('path');
+var bodyParser 		= require('body-parser');
+var ApiV1 			= require('./modules/APIV1');
 
-var App = function(){
+var ServerApp = function(){
 
 	// Scope
 	var self = this;
 
 	// Setup
-	self.dbServer = new mongodb.Server(process.env.OPENSHIFT_MONGODB_DB_HOST, parseInt(process.env.OPENSHIFT_MONGODB_DB_PORT));
-	self.db = new mongodb.Db(process.env.OPENSHIFT_APP_NAME, self.dbServer, {auto_reconnect: true});
+	var mongoHost = process.env.OPENSHIFT_MONGODB_DB_HOST || 'localhost';
+	var mongoPort = process.env.OPENSHIFT_MONGODB_DB_PORT || '27017';
+	var mongoDB	  = process.env.OPENSHIFT_APP_NAME 		  || 'mykanban';
+		
+	self.dbServer = new mongodb.Server(mongoHost, parseInt(mongoPort));
+	self.db = new mongodb.Db(mongoDB, self.dbServer, {auto_reconnect: true});
 	self.dbUser = process.env.OPENSHIFT_MONGODB_DB_USERNAME;
 	self.dbPass = process.env.OPENSHIFT_MONGODB_DB_PASSWORD;
 
@@ -27,34 +29,33 @@ var App = function(){
 		console.warn('No OPENSHIFT_NODEJS_IP environment variable');
 	};
 
-
-
-	// Webapp urls
-	self.app  = express();
-	//self.app.use(bodyParser);
-	//self.app.use(express.methodOverride());
-	//self.app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-	self.app.use(express.static(path.join(__dirname, 'static')));
-	self.app.get('/health', function(req, res){ res.send('1'); });
-	//self.app.use('/', route_index);
-	//self.app.use('/api/v1/', route_mkDropLink);
-
 	// Logic to open a database connection. We are going to call this outside of
 	// app so it is available to all our functions inside.
-
 	self.connectDb = function(callback){
 		self.db.open(function(err, db){
 			if(err){ throw err };
-			self.db.authenticate(self.dbUser, self.dbPass, {authdb: "admin"},  function(err, res){
-				if(err){ throw err };
+			if (self.dbUser) { //if a mongo user is configured
+				self.db.authenticate(self.dbUser, self.dbPass, {authdb: "admin"},  function(err, res){
+					if(err){ throw err };
+					callback();
+				});
+			} else {
 				callback();
-			});
+			}
 		});
 	};
 
 	// starting the nodejs server with express
-
-	self.startServer = function(){
+	self.startServer = function() {
+		// Webapp urls
+		self.app  = express();
+		self.app.use(bodyParser.json());
+		//self.app.use(express.methodOverride());
+		//self.app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+		self.app.use(express.static(path.join(__dirname, 'static')));
+		self.app.get('/health', function(req, res){ res.send('1'); });
+		self.app.use('/api/v1/', (new ApiV1(self.db, '/api/v1/')).router);
+		
 		self.app.listen(self.port, self.ipaddr, function(){
 			console.log('%s: Node server started on %s:%d ...', Date(Date.now()), self.ipaddr, self.port);
 		});
@@ -70,17 +71,23 @@ var App = function(){
 	};
 
 	process.on('exit', function() { self.terminator(); });
-
-	self.terminatorSetup = function(element, index, array) {
-		process.on(element, function() { self.terminator(element); });
-	};
-
-	['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT', 'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGPIPE', 'SIGTERM'].forEach(self.terminatorSetup);
-
+	['SIGHUP', 
+	 'SIGINT', 
+	 'SIGQUIT', 
+	 'SIGILL', 
+	 'SIGTRAP', 
+	 'SIGABRT', 
+	 'SIGBUS', 
+	 'SIGFPE', 
+	 'SIGUSR1', 
+	 'SIGSEGV',
+	 'SIGUSR2', 
+	 'SIGPIPE', 
+	 'SIGTERM'].forEach(function(element, index, array) {
+		 process.on(element, function() { self.terminator(element); });
+	});
 };
 
-//make a new express app
-var app = new App();
-
 //call the connectDb function and pass in the start server command
-app.connectDb(app.startServer);
+var serverApp = new ServerApp();
+serverApp.connectDb(serverApp.startServer);
